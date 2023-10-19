48ca53e74b2082d217374f1044214f4a958cc215
ANKIT KUMAR JHA <116744896+AnkitJha13@users.noreply.github.com>
2023-10-20 00:15:55 +0530

    Update MovieCard.jsx

--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -1,33 +1,52 @@
 import React from "react";
 
 const MovieCard = ({ movie }) => {
-    console.log("Movie object:", movie);
-    return (
-        <div className="movie">
-            <div>
-                <p>{movie.Year}</p>
-            </div>
+  console.log("Movie object:", movie);
 
-            <div>
-                <img
-                    src={
-                        movie.Poster !== "N/A"
-                            ? movie.Poster
-                            : "https://via.placeholder.com/400"
-                    }
-                    alt={movie.title}
-                />
-            </div>
+  // IMDb link for the movie
+  const imdbLink = `https://www.imdb.com/title/${movie.imdbID}/`;
 
-            <div className="movie-info">
-                <span className="movie-type">{movie.Type}</span>
-                <h3 className="movie-title">{movie.Title}</h3>
-                {movie.imdbRating && (
-                    <p className="movie-rating">IMDb Rating: {movie.imdbRating}</p>
-                )}
-            </div>
-        </div>
-    );
+  return (
+    <div className="movie">
+      <div>
+        <p>{movie.Year}</p>
+      </div>
+
+      <div>
+        <img
+          src={
+            movie.Poster !== "N/A"
+              ? movie.Poster
+              : "https://via.placeholder.com/400"
+          }
+          alt={movie.Title}
+        />
+      </div>
+
+      <div className="movie-info">
+        <span className="movie-type">{movie.Type}</span>
+        <h3 className="movie-title">{movie.Title}</h3>
+        {movie.imdbRating && (
+          <p className="movie-rating">IMDb Rating: {movie.imdbRating}</p>
+        )}
+
+        {/* Box Office Collection */}
+        {movie.BoxOffice && (
+          <p className="movie-box-office">Box Office: {movie.BoxOffice}</p>
+        )}
+
+        {/* IMDb Link */}
+        <a
+          href={imdbLink}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="movie-imdb-link"
+        >
+          IMDb Link
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default MovieCard;
