01d213a5af999a9ef4fb1b286fd66c5e7bddae7c
ANKIT KUMAR JHA <116744896+AnkitJha13@users.noreply.github.com>
2023-12-08 00:11:48 +0530

    Update MovieCard.jsx

--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -1,10 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+
+const API_URL = 'https://www.omdbapi.com?apikey=4084c83e';
 
 const MovieCard = ({ movie }) => {
-  console.log("Movie object:", movie);
+  const [genres, setGenres] = useState([]);
 
-  // IMDb link for the movie
+  useEffect(() => {
+    const fetchGenres = async () => {
+      try {
+        const response = await fetch(`${API_URL}&i=${movie.imdbID}`);
+        const movieData = await response.json();
+        
+        if (movieData.Genre) {
+          const movieGenres = movieData.Genre.split(',').map((genre) => genre.trim());
+          setGenres(movieGenres);
+        }
+      } catch (error) {
+        console.error('Error fetching movie genres:', error);
+      }
+    };
+
+    fetchGenres();
+  }, [movie.imdbID]);
+
   const imdbLink = `https://www.imdb.com/title/${movie.imdbID}/`;
+  const youtubeLink = `https://www.youtube.com/results?search_query=${encodeURIComponent(
+    `${movie.Title} ${movie.Year} trailer`
+  )}`;
 
   return (
     <div className="movie">
@@ -26,16 +48,19 @@ const MovieCard = ({ movie }) => {
       <div className="movie-info">
         <span className="movie-type">{movie.Type}</span>
         <h3 className="movie-title">{movie.Title}</h3>
+
+        {genres.length > 0 && (
+          <p className="movie-genres">Genres: {genres.join(', ')}</p>
+        )}
+
         {movie.imdbRating && (
           <p className="movie-rating">IMDb Rating: {movie.imdbRating}</p>
         )}
 
-        {/* Box Office Collection */}
         {movie.BoxOffice && (
           <p className="movie-box-office">Box Office: {movie.BoxOffice}</p>
         )}
 
-        {/* IMDb Link */}
         <a
           href={imdbLink}
           target="_blank"
@@ -43,6 +68,15 @@ const MovieCard = ({ movie }) => {
           className="movie-imdb-link"
         >
           IMDb Link
+        </a>
+
+        <a
+          href={youtubeLink}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="movie-trailer-link"
+        >
+          Watch Trailer
         </a>
       </div>
     </div>
