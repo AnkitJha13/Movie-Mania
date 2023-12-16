a877ebc470240c45ac48998aba899de3394f06db
ANKIT KUMAR JHA <116744896+AnkitJha13@users.noreply.github.com>
2023-12-16 20:50:20 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -49,7 +49,7 @@ const App = () => {
 
   return (
     <div className="app">
-      <h1>MovieMania</h1>
+      <h1>Movie Mania</h1>
       <div className="search">
         <input
           placeholder="Search for movies"
