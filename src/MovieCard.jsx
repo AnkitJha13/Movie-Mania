d13347fc3b5975419f20f77172770659e08fead8
ANKIT KUMAR JHA <116744896+AnkitJha13@users.noreply.github.com>
2023-12-17 00:06:03 +0530

    Update MovieCard.jsx

--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -2,17 +2,22 @@ import React, { useState, useEffect } from 'react';
 
 const API_URL = 'https://www.omdbapi.com?apikey=4084c83e';
 
-const MovieCard = ({ movie }) => {
+const MovieCard = ({ movie }) => {       // takes a movie prop as an argument
+  
+  // State to hold the movie genres and hover state
   const [genres, setGenres] = useState([]);
   const [isHovered, setIsHovered] = useState(false);
 
-  useEffect(() => {
+  // useEffect to fetch movie genres when the component mounts or movie changes
+  useEffect(() => {         //to fetch movie genres when the component mounts or when the movie.imdbID changes.
     const fetchGenres = async () => {
       try {
+        // Fetch movie data using IMDb ID
         const response = await fetch(`${API_URL}&i=${movie.imdbID}`);
         const movieData = await response.json();
 
         if (movieData.Genre) {
+          // Extract genres from the response and update state
           const movieGenres = movieData.Genre.split(',').map((genre) => genre.trim());
           setGenres(movieGenres);
         }
@@ -20,15 +25,17 @@ const MovieCard = ({ movie }) => {
         console.error('Error fetching movie genres:', error);
       }
     };
-
+    // Call fetchGenres function
     fetchGenres();
-  }, [movie.imdbID]);
+  }, [movie.imdbID]); // Dependency array to re-run effect when movie.imdbID changes
 
+  // IMDb and YouTube links based on movie details
   const imdbLink = `https://www.imdb.com/title/${movie.imdbID}/`;
   const youtubeLink = `https://www.youtube.com/results?search_query=${encodeURIComponent(
     `${movie.Title} ${movie.Year} trailer`
   )}`;
 
+   // Render the MovieCard component
   return (
     <div
       className={`movie ${isHovered ? 'hovered' : ''}`}
