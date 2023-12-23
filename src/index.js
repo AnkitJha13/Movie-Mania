926e2cdad0c744a59117e59df73d6d0c60a4bad2
ANKIT KUMAR JHA <116744896+AnkitJha13@users.noreply.github.com>
2023-12-23 22:32:12 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from 'react-dom/client';
 
 import App from './App';
 
-const container = document.getElementById('root'); // The root represents the entry point for rendering React components into the DOM. It takes the 'container' as an argument.
+const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
 
-root.render(  // This line initiates the rendering process for the React application.
+root.render(
     <React.StrictMode>
         <App />
     </React.StrictMode>
