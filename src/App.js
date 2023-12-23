e9c5b55807fb01580ebb7c3a918001b95a4d0fef
ANKIT KUMAR JHA <116744896+AnkitJha13@users.noreply.github.com>
2023-12-23 22:31:20 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,35 +1,29 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import MovieCard from './MovieCard';
+import Popup from './Popup';
 import './App.css';
 import SearchIcon from './search.svg';
 
-const API_URL = 'https://www.omdbapi.com?apikey=4084c83e';
+export const API_URL = 'https://www.omdbapi.com?apikey=4084c83e';
 
 const App = () => {
-  
-  // State to hold the list of movies and the search term
   const [movies, setMovies] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
-  
-  // Function to search for movies based on the provided title
+  const [showPopup, setShowPopup] = useState(false);
+  const [popupLink, setPopupLink] = useState('');
+
   const searchMovies = async (title) => {
     try {
-
-      // Fetch movie data from OMDB API using the provided title
       const response = await fetch(`${API_URL}&s=${title}`);
       const data = await response.json();
 
-      // Check if the search returned any results
       if (data.Search) {
-
-        // Fetch additional details for each movie, including IMDb rating and Box Office
         const moviesWithRatings = await Promise.all(
           data.Search.map(async (movie) => {
             try {
               const response = await fetch(`${API_URL}&i=${movie.imdbID}`);
               const movieData = await response.json();
 
-              // Add IMDb rating and Box Office details to the movie object
               return {
                 ...movie,
                 imdbRating: movieData.imdbRating,
@@ -37,32 +31,45 @@ const App = () => {
               };
             } catch (error) {
               console.error('Error fetching movie details:', error);
-              return movie; // Return the original movie object if there is an error fetching details
+              return movie;
             }
           })
         );
 
-        // Update the movies state with the enhanced movie data
         setMovies(moviesWithRatings);
       } else {
-        // If no results were found, set movies state to an empty array
         setMovies([]);
       }
     } catch (error) {
       console.error('Error searching movies:', error);
-      setMovies([]);  // Set movies state to an empty array in case of an error
+      setMovies([]);
     }
   };
 
-  // useEffect hook to initially search for movies when the component mounts
+  const handlePopupClose = () => {
+    setShowPopup(false);
+    setPopupLink('');
+  };
+
+  const handlePopupConfirm = () => {
+    if (popupLink) {
+      window.open(popupLink, '_blank');
+    }
+    handlePopupClose();
+  };
+
+  const handleOpenPopup = (link) => {
+    setPopupLink(link);
+    setShowPopup(true);
+  };
+
   useEffect(() => {
     searchMovies('Batman');
   }, []);
 
-  // Render the App component
   return (
     <div className="app">
-      <h1>Movie Mania</h1>
+      <h1 className="glowing-text">Movie Mania</h1>
       <div className="search">
         <input
           placeholder="Search for movies"
@@ -79,13 +86,22 @@ const App = () => {
       {movies?.length > 0 ? (
         <div className="container">
           {movies.map((movie, index) => (
-            <MovieCard key={index} movie={movie} />
+            <MovieCard
+              key={index}
+              movie={movie}
+              onOpenPopup={() => handleOpenPopup(`https://www.imdb.com/title/${movie.imdbID}/`)}
+              onWatchTrailer={() => handleOpenPopup(`https://www.youtube.com/results?search_query=${encodeURIComponent(`${movie.Title} ${movie.Year} trailer`)}`)}
+            />
           ))}
         </div>
       ) : (
         <div className="empty">
           <h2>No movies found</h2>
         </div>
+      )}
+
+      {showPopup && (
+        <Popup onClose={handlePopupClose} onConfirm={handlePopupConfirm} />
       )}
     </div>
   );
